c3b9ccb1da56e70b870ee2645958996df1d0a892
SamGrant98 <samgrantarab@gmail.com>
2019-10-08 18:41:29 +0100

    things

--- dist/modern-javascript-002-SamGrant98.e31bb0bc.js
+++ dist/modern-javascript-002-SamGrant98.e31bb0bc.js
@@ -160,7 +160,7 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; if (obj != null) { var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 var things = ["Kraken Dark Rum", "OVD Rum", "Absolute Vodka", "Absolute Lime Vodka"];
-(0, _renderer.render)(data());
+(0, _renderer.render)(things);
 },{"./renderer":"renderer.js","./data.json":"data.json"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import * as Data from "./data.json";
 
 const things = ["Kraken Dark Rum", "OVD Rum", "Absolute Vodka", "Absolute Lime Vodka"];
   
-render(data());
+render(things);
