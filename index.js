31f2a01cb01f5b25ca2a3da611bc5329d1b02693
SamGrant98 <samgrantarab@gmail.com>
2019-10-08 18:56:12 +0100

    content

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ import {render} from "./renderer";
 import * as Data from "./data.json";
 
 const things = ["Kraken Dark Rum", "OVD Rum", "Absolute Vodka", "Absolute Lime Vodka"];
-  
-render(things);
+let content = "";
+
+render(content);
+
+for(let i=0; 1 < things.length; i++)
+{
+    console.log(things[i]);
+    content += things[i].name;
+}
